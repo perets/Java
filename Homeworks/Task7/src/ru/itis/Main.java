2cef0ec9fd07deee2efe91000f773015a7dfea51
Усанов Александр Иванович <usanov.a.i@korston.org>
2017-10-26 12:36:28 +0300

    add Solution task7

--- Homeworks/Task7/src/ru/itis/Main.java
+++ Homeworks/Task7/src/ru/itis/Main.java
@@ -5,10 +5,26 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Random random = new Random();
-        Rectangle[] rectangles = new Rectangle[5];
-        for (int i = 0; i < 5; i++){
-            rectangles[i] = new Rectangle(random.nextInt(10), random.nextInt(10));
-            System.out.println(rectangles[i].area());
+        Rectangle[] rectangles = new Rectangle[10];
+        for (int i = 0; i < 10; i++){
+            rectangles[i] = new Rectangle(random.nextInt(10) + 1, random.nextInt(10) + 1);
+            rectangles[i].print();
+        }
+
+        for (int i = 0;  i < rectangles.length; i++) {
+            int indexMin = i;
+            for (int j = i; j < rectangles.length; j++){
+                if (rectangles[j].area() < rectangles[indexMin].area()) {
+                    indexMin = j;
+                }
+            }
+            Rectangle temp = rectangles[i];
+            rectangles[i] = rectangles[indexMin];
+            rectangles[indexMin] = temp;
+        }
+        System.out.println("Сортировка по возрастанию площади: ");
+        for (int i = 0; i < 10; i++){
+            rectangles[i].print();
         }
 
     }
--- Homeworks/Task7/src/ru/itis/Rectangle.java
+++ Homeworks/Task7/src/ru/itis/Rectangle.java
@@ -12,4 +12,9 @@ public class Rectangle {
     public int area(){
         return lenght * widht;
     }
+
+    public void print(){
+        System.out.println("Длина: " + this.lenght + ". "
+                + "Ширина: " + this.widht + ". " + "Площадь: " + this.area());
+    }
 }
